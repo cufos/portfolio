88368762cdfe8191cc9a1eeab5e501d9a7364cd3
Juni <yunior.varon@yahoo.com>
2021-01-27 12:29:57 +0100

    trying the pwa

--- components/seo.js
+++ components/seo.js
@@ -4,7 +4,7 @@ export default function Seo() {
   return (
     <Head>
       <title> Junior Varon | Frontend, Javascript, React, CSS </title>
-      <meta name="description" content=" Desarrollo Web, Frontend, Javascript, React ⚛️, CSS" key='description' />
+      <meta name="description" content="Web development, Frontend, Javascript, React ⚛️, CSS" key='description' />
       <link rel="canonical" href="https://junior-portfolio.vercel.app/"></link>
       <link rel="icon" href="/robot.png" />
       <link rel="apple-touch-icon" sizes="180x180" href="robot.png"></link>
@@ -17,8 +17,17 @@ export default function Seo() {
       <meta property="og:locale" content="en_EN" />
       <meta property="og:title" content="Junior Varon | Frontend, Javascript, React, CSS" />
       <meta property="og:image" content="/robot.png" />
-      <meta property="og:description" content="Desarrollo Web, Frontend, Javascript, React ⚛️, CSS" />
+      <meta property="og:description" content="Web development, Frontend, Javascript, React ⚛️, CSS" />
       <meta property="og:site_name" content="junior-portfolio.vercel.app" />
+      <meta name='keywords' content='front-end,web,developer,react,css,javascript' />
+
+      <link rel="manifest" href="/manifest.json" />
+      <link href='/favicon-16x16.png' rel='icon' type='robot/png' sizes='16x16' />
+      <link href='/favicon-32x32.png' rel='icon' type='robot/png' sizes='32x32' />
+      <meta name="theme-color" content="#07dac0" />
+      <meta name='apple-mobile-web-app-title' content='Junior Portfolio' />
+      <meta name='apple-mobile-web-app-capable' content='yes' />
+      <meta name='apple-mobile-web-app-status-bar-style' content='#07dac0' />
     </Head>
   )
 }
